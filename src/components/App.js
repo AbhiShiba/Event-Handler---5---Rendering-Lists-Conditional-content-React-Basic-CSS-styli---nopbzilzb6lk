096d7e9533c8bdbdd99481e609e0033b8fa93d3c
Abhishek <55306595+AbhiShiba@users.noreply.github.com>
2022-11-29 20:54:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-   const eventHandler = (event) {
+   const eventHandler = (event) => {
     event.preventDefault();
      console.log("form submitted");
    }
